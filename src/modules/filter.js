d779e85f4dbeee79ea2a0fee5dde39d72230aa03
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2022-02-28 13:58:03 +0300

    filter
    filter

--- src/modules/filter.js
+++ src/modules/filter.js
@@ -1,11 +1,49 @@
 const filter = () => {
     let select = document.querySelector('select');
-    let cardSam = document.querySelectorAll('.samokat')
-    let cardSeg = document.querySelectorAll('.segway')
-    let cardGyr = document.querySelectorAll('.gyro')
+    let cardSams = document.querySelectorAll('.samokat')
+    let cardSegs = document.querySelectorAll('.segway')
+    let cardGyrs = document.querySelectorAll('.gyro')
     select.addEventListener('change', () => {
       if (select.value === 'filter1') {
-      } else if (select.value == 'filter2') {
+        for (let cardSam of cardSams) {
+            cardSam.classList.remove('d-none')
+        }
+        for (let cardSeg of cardSegs) {
+            cardSeg.classList.remove('d-none')
+        }
+        for (let cardGyr of cardGyrs) {
+            cardGyr.classList.remove('d-none')
+        }
+      } else if (select.value === 'filter2') {
+        for (let cardSam of cardSams) {
+            cardSam.classList.add('d-none')
+        }
+        for (let cardSeg of cardSegs) {
+            cardSeg.classList.add('d-none')
+        }
+        for (let cardGyr of cardGyrs) {
+            cardGyr.classList.remove('d-none')
+        }
+      } else if (select.value === 'filter3') {
+        for (let cardSam of cardSams) {
+            cardSam.classList.remove('d-none')
+        }
+        for (let cardSeg of cardSegs) {
+            cardSeg.classList.add('d-none')
+        }
+        for (let cardGyr of cardGyrs) {
+            cardGyr.classList.add('d-none')
+        }
+      } else if (select.value === 'filter4') {
+        for (let cardSam of cardSams) {
+            cardSam.classList.add('d-none')
+        }
+        for (let cardSeg of cardSegs) {
+            cardSeg.classList.remove('d-none')
+        }
+        for (let cardGyr of cardGyrs) {
+            cardGyr.classList.add('d-none')
+        }
       }
     });
 }
