3c7cbc7a0597b8a06e83948d9651dcb2157763f6
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2022-02-28 12:28:38 +0300

    webpack + scroll
    webpack + scroll

---
+++ src/modules/scroll.js
@@ -0,0 +1,14 @@
+const scroll = () => {
+    const smoothLinks = document.querySelectorAll('a[href^="#up"]');
+    for (let smoothLink of smoothLinks) {
+        smoothLink.addEventListener('click', function (e) {
+            e.preventDefault();
+            const id = smoothLink.getAttribute('href');
+            document.querySelector(id).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    };
+  }
+  export default scroll
