949f2d66f5a3a46c94cbe4951f2136eb36a2b8a8
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2022-03-01 12:33:01 +0300

    db робит фильтер нет
    db робит фильтер нет

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import scroll from './modules/scroll'
 import modal from './modules/modal'
 import slider from './modules/slider'
 import filter from './modules/filter'
+import db from './modules/db'
 
 scroll()
 modal()
 slider()
 filter()
+db()
---
+++ src/modules/db.js
@@ -0,0 +1,38 @@
+const db = () => {
+  let cards = document.querySelector('.cards'),
+  requestURL = '../db/hero.json',
+  request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.onload = function(e) {
+  if (request.readyState === 4) {
+      if (request.status === 200) {
+          // console.log(request.response);
+          let dataCards = JSON.parse(request.responseText);
+          getTableInfo(dataCards);
+      } else {
+          console.error(request.statusText);
+      }
+  }
+};
+request.send();
+
+function getTableInfo(data) {
+  data.forEach(function(item){
+    cards.innerHTML += 
+    `
+    <div class="card card__item">
+    <img src="${item.фото}" class="card__img" alt="card__img">
+    <h4 class="card__name">${item.название}</h4>
+    <span class="card__type">${item.тип}</span>
+    <span class="card__battery">${item.батарея}</span>
+    <span class="card__power">${item.мощность}</span>
+    <span class="card__wheel">${item.колёса}</span>
+    <span class="card__weight">${item.вес}</span>
+    <p class="card__info">${item.описание}</p>
+    </div>
+    `
+    // console.log(item.название);
+  })
+}
+}
+export default db
